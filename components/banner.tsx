1695836e0a6a6795db46edfc740cdca28d1d2944
adefope oluwatosin <sirtosin45@gmail.com>
2022-11-10 07:20:25 +0000

    update

--- components/banner.tsx
+++ components/banner.tsx
@@ -1,7 +1,7 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 function Banner({}: Props) {
   return (
@@ -10,17 +10,18 @@ function Banner({}: Props) {
         src="https://links.papareact.com/0fm"
         layout="fill"
         objectFit="cover"
+        alt="btn"
       />
       <div className="absolute top-1/2 w-full text-center">
         <p className="text-sm capitalize sm:text-lg">
           not sure where to go? perfect.
         </p>
         <button className="font-bold my-3 bg-white text-purple-500 py-4 px-10 rounded-full shadow-md hover:shadow-xl active:scale-90 transition duration-150 ease-out ">
-          I'm flexible
+          I am flexible
         </button>
       </div>
     </div>
   );
 }
 
-export default Banner
+export default Banner;
